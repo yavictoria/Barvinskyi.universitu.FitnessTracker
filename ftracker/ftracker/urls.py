23ef43af072e85585e85774e7bf9549aa8f7b185
VladBarvinskyi323 <116297199+VladBarvinskyi323@users.noreply.github.com>
2024-03-04 18:32:24 +0200

    First step for Google Oauth

--- ftracker/ftracker/urls.py
+++ ftracker/ftracker/urls.py
@@ -1,7 +1,7 @@
 """
-URL configuration for ftracker project.
+URL configuration for CityVotingPlatform project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The urlpatterns list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import ftracker.mainapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', ftracker.mainapp.views.register, name='register'),
+    path("ftracker_registration/", include("allauth.urls")),
+    path('accounts/google/login/callback/', ftracker.mainapp.views.callback_view, name='google_callback'),
+    path('', ftracker.mainapp.views.home, name='home'),  # Home page
 ]
--- ftracker/mainapp/views.py
+++ ftracker/mainapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 # Create your views here.
+
+
+# Create your views here.
+def home(request):
+    return render(request, "homepage.html")
+
+
+def register(request):
+    return render(request, "register.html")
+
+
+def callback_view(request):
+    return redirect(reverse('home'))
